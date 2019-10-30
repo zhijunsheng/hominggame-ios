a980a4a85b543dafafccd29df3aabb274391ff93
xiaobao <mariuspuzhouli@gmail.com>
2019-10-30 20:05:38 +0800

    we've a dice

--- HomingGame/ViewController.swift
+++ HomingGame/ViewController.swift
@@ -2,10 +2,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var oneToSixLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        oneToSixLabel.text = "1"
     }
-
-
+    
+    @IBAction func change(_ sender: Any) {
+        let oneToSix = arc4random() % 6 + 1
+        oneToSixLabel.text = "\(oneToSix)"
+    }
+    
 }
 
