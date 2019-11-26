38370136e4b7335c3d844ef11d0ea363cc7b3ef5
xiaobao <mariuspuzhouli@gmail.com>
2019-11-26 20:18:11 +0800

    we did the WhosTurnLabel

--- HomingGame/ViewController.swift
+++ HomingGame/ViewController.swift
@@ -5,6 +5,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var oneToSixLabel: UILabel!
     @IBOutlet weak var whosTurnLabel: UILabel!
     
+    
     var pieceEngine = PieceEngine()
     var whosTurn = PlayerColor.red
     
@@ -23,6 +24,20 @@ class ViewController: UIViewController {
         pieceEngine.movePiece(piece: whosTurn, meter: oneToSix)
         boardView.shadowPieceBox = pieceEngine.pieceBox
         boardView.setNeedsDisplay()
+        if whosTurn == PlayerColor.red {
+            whosTurn = PlayerColor.yellow
+            whosTurnLabel.text = "Yellow Player's Turn"
+        } else if whosTurn == PlayerColor.yellow {
+            whosTurn = PlayerColor.blue
+            whosTurnLabel.text = "Blue Player's Turn"
+        } else if whosTurn == PlayerColor.blue {
+            whosTurn = PlayerColor.green
+            whosTurnLabel.text = "Green Player's Turn"
+        } else if whosTurn == PlayerColor.green {
+            whosTurn = PlayerColor.red
+            whosTurnLabel.text = "Red Player's Turn"
+        }
+        
     }
     
 }
